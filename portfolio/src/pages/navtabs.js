83eca8feede4c550a9d6b4e38b1c63a357a72c74
Joel Prentiss <joel.prentiss89@gmail.com>
2022-09-13 12:38:50 -0500

    test 1

--- portfolio/src/pages/navtabs.js
+++ portfolio/src/pages/navtabs.js
@@ -6,7 +6,7 @@ function NavTabs({currentPage, handlePageChange}){
         <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div className="container">
              <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
+           npm star         Menu
                     <i className="fas fa-bars"></i>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
